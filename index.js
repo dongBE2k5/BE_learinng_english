93e150936291ef3c2e0aa99686b4b6ec00628ef6
Đồng <yivictor2005@gmail.com>
2026-08-15 13:59:30 +0700

    fix: update dbConfig and bind host for render

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ async function startServer() {
     console.log('Initializing database...');
     await initDB();
     
-    app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+        console.log(`Server is running on port ${PORT}`);
     });
 }
 
